a5c7c286a5a9fffb188ef91d4e20cf6220e246d4
TryptophanDealer <advena.art@gmail.com>
2022-11-25 20:16:43 +0100

    final touches

--- PhoneApp/initiate.js
+++ PhoneApp/initiate.js
@@ -172,9 +172,10 @@ if (trueCredit < 0) {popUp(4)}
         document.body.style.color = "rgb(206,130,60)"
     }
 
+        document.documentElement.scrollTop = 0
         isFinished = true
         document.getElementById("finishBtn").classList.toggle("hide")
-        endFieldFiller()        
+        endFieldFiller()
 }
 };
 
